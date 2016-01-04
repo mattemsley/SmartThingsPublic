b822229d38059361876700f27affee651db0e31e
Matthew Emsley <mattemsley@hotmail.com>
2016-01-04 15:21:29 -0500

    MKE initial commit

--- smartapps/smartthings/virtual-thermostat.src/virtual-thermostat.groovy
+++ smartapps/smartthings/virtual-thermostat.src/virtual-thermostat.groovy
@@ -10,134 +10,228 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Virtual Thermostat
+ *  Keep Me Cozy II
  *
  *  Author: SmartThings
  */
+
 definition(
     name: "Virtual Thermostat",
     namespace: "smartthings",
-    author: "SmartThings",
-    description: "Control a space heater or window air conditioner in conjunction with any temperature sensor, like a SmartSense Multi.",
+    author: "SmartThings-Matt Emsley",
+    description: "Based on Keep Me Cozy II, but enables specific time window and mode.",
     category: "Green Living",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/temp_thermo-switch.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/temp_thermo-switch@2x.png"
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/temp_thermo.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/temp_thermo@2x.png"
 )
 
-preferences {
-	section("Choose a temperature sensor... "){
-		input "sensor", "capability.temperatureMeasurement", title: "Sensor"
-	}
-	section("Select the heater or air conditioner outlet(s)... "){
-		input "outlets", "capability.switch", title: "Outlets", multiple: true
-	}
-	section("Set the desired temperature..."){
-		input "setpoint", "decimal", title: "Set Temp"
-	}
-	section("When there's been movement from (optional, leave blank to not require motion)..."){
-		input "motion", "capability.motionSensor", title: "Motion", required: false
-	}
-	section("Within this number of minutes..."){
-		input "minutes", "number", title: "Minutes", required: false
-	}
-	section("But never go below (or above if A/C) this value with or without motion..."){
-		input "emergencySetpoint", "decimal", title: "Emer Temp", required: false
-	}
-	section("Select 'heat' for a heater and 'cool' for an air conditioner..."){
-		input "mode", "enum", title: "Heating or cooling?", options: ["heat","cool"]
-	}
+preferences() {
+    page name:"Setup"
+}
+
+def Setup() {
+    def pageProperties = [
+        name:       "Setup",
+        title:      "Setup",
+        nextPage:   null,
+        install:    true,
+        uninstall:  true
+    ]
+    def newMode = [
+        name:       	"newMode",
+        type:       	"mode",
+        title:      	"Which?",
+        multiple:   	true,
+        required:   	true
+    ]
+	return dynamicPage(pageProperties) {
+        section("Choose thermostat... ") {
+            input "thermostat", "capability.thermostat"
+        }
+        section("Heat setting..." ) {
+            input "heatingSetpoint", "decimal", title: "Degrees"
+        }
+        section("Air conditioning setting...") {
+            input "coolingSetpoint", "decimal", title: "Degrees"
+        }
+        section("Optionally choose temperature sensor to use instead of the thermostat's... ") {
+            input "sensor", "capability.temperatureMeasurement", title: "Temp Sensors", required: false
+        }
+        section("Which mode change triggers the simulator? (This app will only run in selected mode(s))") {
+            input newMode           
+        }
+        section("What time window?") {
+    	        href "timeIntervalInput", title: "Only during a certain time", description: getTimeLabel(starting, ending), state: greyedOutTime(starting, ending), refreshAfterSelection:true
+    	}
+    }
 }
 
 def installed()
 {
-	subscribe(sensor, "temperature", temperatureHandler)
-	if (motion) {
-		subscribe(motion, "motion", motionHandler)
-	}
+	log.debug "enter installed, state: $state"
+	subscribeToEvents()
 }
 
 def updated()
 {
+	log.debug "enter updated, state: $state"
 	unsubscribe()
-	subscribe(sensor, "temperature", temperatureHandler)
-	if (motion) {
-		subscribe(motion, "motion", motionHandler)
+	subscribeToEvents()
+}
+
+def subscribeToEvents()
+{
+	subscribe(location, changedLocationMode)
+	if (sensor) {
+		subscribe(sensor, "temperature", temperatureHandler)
+		subscribe(thermostat, "temperature", temperatureHandler)
+		subscribe(thermostat, "thermostatMode", temperatureHandler)
 	}
+	evaluate()
+}
+
+def changedLocationMode(evt)
+{
+	log.debug "changedLocationMode mode: $evt.value, heat: $heat, cool: $cool"
+	evaluate()
 }
 
 def temperatureHandler(evt)
 {
-	def isActive = hasBeenRecentMotion()
-	if (isActive || emergencySetpoint) {
-		evaluate(evt.doubleValue, isActive ? setpoint : emergencySetpoint)
-	}
-	else {
-		outlets.off()
-	}
+	evaluate()
 }
 
-def motionHandler(evt)
+private evaluate()
 {
-	if (evt.value == "active") {
-		def lastTemp = sensor.currentTemperature
-		if (lastTemp != null) {
-			evaluate(lastTemp, setpoint)
-		}
-	} else if (evt.value == "inactive") {
-		def isActive = hasBeenRecentMotion()
-		log.debug "INACTIVE($isActive)"
-		if (isActive || emergencySetpoint) {
-			def lastTemp = sensor.currentTemperature
-			if (lastTemp != null) {
-				evaluate(lastTemp, isActive ? setpoint : emergencySetpoint)
+if(allOk){
+	log.debug("Keep Me Cozy II-Time Window Running")
+	if (sensor) {
+		def threshold = 1.0
+		def tm = thermostat.currentThermostatMode
+		def ct = thermostat.currentTemperature
+		def currentTemp = sensor.currentTemperature
+		log.trace("evaluate:, mode: $tm -- temp: $ct, heat: $thermostat.currentHeatingSetpoint, cool: $thermostat.currentCoolingSetpoint -- "  +
+			"sensor: $currentTemp, heat: $heatingSetpoint, cool: $coolingSetpoint")
+		if (tm in ["cool","auto"]) {
+			// air conditioner
+			if (currentTemp - coolingSetpoint >= threshold) {
+				thermostat.setCoolingSetpoint(ct - 2)
+				log.debug "thermostat.setCoolingSetpoint(${ct - 2}), ON"
+			}
+			else if (coolingSetpoint - currentTemp >= threshold && ct - thermostat.currentCoolingSetpoint >= threshold) {
+				thermostat.setCoolingSetpoint(ct + 2)
+				log.debug "thermostat.setCoolingSetpoint(${ct + 2}), OFF"
 			}
 		}
-		else {
-			outlets.off()
-		}
-	}
-}
-
-private evaluate(currentTemp, desiredTemp)
-{
-	log.debug "EVALUATE($currentTemp, $desiredTemp)"
-	def threshold = 1.0
-	if (mode == "cool") {
-		// air conditioner
-		if (currentTemp - desiredTemp >= threshold) {
-			outlets.on()
-		}
-		else if (desiredTemp - currentTemp >= threshold) {
-			outlets.off()
-		}
-	}
-	else {
-		// heater
-		if (desiredTemp - currentTemp >= threshold) {
-			outlets.on()
-		}
-		else if (currentTemp - desiredTemp >= threshold) {
-			outlets.off()
-		}
-	}
-}
-
-private hasBeenRecentMotion()
-{
-	def isActive = false
-	if (motion && minutes) {
-		def deltaMinutes = minutes as Long
-		if (deltaMinutes) {
-			def motionEvents = motion.eventsSince(new Date(now() - (60000 * deltaMinutes)))
-			log.trace "Found ${motionEvents?.size() ?: 0} events in the last $deltaMinutes minutes"
-			if (motionEvents.find { it.value == "active" }) {
-				isActive = true
+		if (tm in ["heat","emergency heat","auto"]) {
+			// heater
+			if (heatingSetpoint - currentTemp >= threshold) {
+				thermostat.setHeatingSetpoint(ct + 2)
+				log.debug "thermostat.setHeatingSetpoint(${ct + 2}), ON"
+			}
+			else if (currentTemp - heatingSetpoint >= threshold && thermostat.currentHeatingSetpoint - ct >= threshold) {
+				thermostat.setHeatingSetpoint(ct - 2)
+				log.debug "thermostat.setHeatingSetpoint(${ct - 2}), OFF"
 			}
 		}
 	}
 	else {
-		isActive = true
+		thermostat.setHeatingSetpoint(heatingSetpoint)
+		thermostat.setCoolingSetpoint(coolingSetpoint)
+		thermostat.poll()
 	}
-	isActive
+    }
 }
 
+//below is used to check restrictions
+private getAllOk() {
+	modeOk && timeOk
+}
+
+private getModeOk() {
+	def result = !newMode || newMode.contains(location.mode)
+	log.trace "modeOk = $result"
+	result
+}
+
+private getTimeOk() {
+	def result = true
+	if (starting && ending) {
+		def currTime = now()
+		def start = timeToday(starting).time
+		def stop = timeToday(ending).time
+		result = start < stop ? currTime >= start && currTime <= stop : currTime <= stop || currTime >= start
+	}
+    
+    else if (starting){
+    	result = currTime >= start
+    }
+    else if (ending){
+    	result = currTime <= stop
+    }
+    
+	log.trace "timeOk = $result"
+	result
+}
+
+def getTimeLabel(starting, ending){
+
+	def timeLabel = "Tap to set"
+	
+    if(starting && ending){
+    	timeLabel = "Between" + " " + hhmm(starting) + " "  + "and" + " " +  hhmm(ending)
+    }
+    else if (starting) {
+		timeLabel = "Start at" + " " + hhmm(starting)
+    }
+    else if(ending){
+    timeLabel = "End at" + hhmm(ending)
+    }
+	timeLabel
+}
+
+private hhmm(time, fmt = "h:mm a")
+{
+	def t = timeToday(time, location.timeZone)
+	def f = new java.text.SimpleDateFormat(fmt)
+	f.setTimeZone(location.timeZone ?: timeZone(time))
+	f.format(t)
+}
+def greyedOut(){
+	def result = ""
+    if (switches) {
+    	result = "complete"	
+    }
+    result
+}
+
+def greyedOutSettings(){
+	def result = ""
+    if (starting || ending ) {
+    	result = "complete"	
+    }
+    result
+}
+
+def greyedOutTime(starting, ending){
+	def result = ""
+    if (starting || ending) {
+    	result = "complete"	
+    }
+    result
+}
+
+// for backward compatibility with existing subscriptions
+def coolingSetpointHandler(evt) {
+	log.debug "coolingSetpointHandler()"
+}
+def heatingSetpointHandler (evt) {
+	log.debug "heatingSetpointHandler ()"
+}
+
+page(name: "timeIntervalInput", title: "Only during a certain time", refreshAfterSelection:true) {
+		section {
+			input "starting", "time", title: "Starting", required: false 
+			input "ending", "time", title: "Ending", required: false 
+		}
+}
